0c10f835cd1c75963c6e3dad96eccda9b4ad547e
ahmadhabibi14 <ahmadhabibi7159@gmail.com>
2023-03-12 22:02:52 +0800

    Update

---
+++ log.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+  "log"
+  "os"
+)
+
+// Why not use package fmt ?
+// 1) Package log is safe from concurrent goroutines while plain fmt isn't
+// 2) Log can attach information automatically, such as time, date, file path, etc.
+func main() {
+  log.Println("Habibi")
+
+  // Ldate uses to get only a date
+  // Lshortfile uses to get filename
+  log.SetFlags(log.Ldate | log.Lshortfile)
+  log.Println("Sup ninja")
+
+  // Panic
+  //log.Panic("Panicking...")
+  //Fatal
+  //log.Fatal("Uh-oh...")
+
+  // Create a file to write logs
+  file, _ := os.Create("file.log")
+  log.SetOutput(file)
+
+  // Print log message to file
+  log.Println("Hello World !")
+  file.Close()
+  log.SetOutput(os.Stdout)
+
+  // Common loggers
+  flags := log.LstdFlags | log.Lshortfile
+  infoLogger := log.New(os.Stdout, "INFO: ", flags)
+  warnLogger := log.New(os.Stdout, "WARN: ", flags)
+  errorLogger := log.New(os.Stdout, "ERROR: ", flags)
+
+  infoLogger.Println("This is an info log")
+  warnLogger.Println("This is an warning log")
+  errorLogger.Println("This is an error log")
+}
