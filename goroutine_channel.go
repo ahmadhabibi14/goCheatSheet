8ef6c8d1d1ee720b036e1cde9d0e2a17c9571ec1
Habibi <ahmadhabibi7159@gmail.com>
2023-05-29 23:25:11 +0800

    update

---
+++ goroutine_channel.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	// create a channel to recieve termination signals
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+
+	// start goroutine to listen all signals
+	go func() {
+		<-signalChan // wait for the signal
+		fmt.Printf("\nReceived termination signal, clean up...")
+		// perform any clean up here
+
+		// exit the application
+		os.Exit(0)
+	}()
+
+	// your cl application here
+	fmt.Println("Print CTRL+C or 'q' to exit.")
+	for {
+		// read user input or perform any necessary operations
+		var input string
+		fmt.Scanln(&input)
+
+		// check for the condition
+		if input == "q" {
+			fmt.Println("Exiting...")
+			os.Exit(0)
+		}
+
+		// process the input or perform other tasks
+		fmt.Printf("You entered: %s\n", input)
+	}
+}
