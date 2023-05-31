573a6c2ebceb7fb075654bc2d2ed3569d0248103
Habibi <ahmadhabibi7159@gmail.com>
2023-05-31 23:41:46 +0800

    sort

---
+++ data-structure/sort_map_by_key.go
@@ -0,0 +1,26 @@
+// Sort map by key
+
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	basket := map[string]int{
+		"orange":     5,
+		"apple":      7,
+		"mango":      3,
+		"strawberry": 9,
+	}
+	keys := make([]string, 0, len(basket))
+	for k := range basket {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		fmt.Println(k, basket[k])
+	}
+}
---
+++ data-structure/sort_map_by_value.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	basket := map[string]int{
+		"orange":     5,
+		"apple":      7,
+		"mango":      3,
+		"strawberry": 9,
+	}
+	keys := make([]string, 0, len(basket))
+	for key := range basket {
+		keys = append(keys, key)
+	}
+
+	sort.SliceStable(keys, func(i, j int) bool {
+		return basket[keys[i]] < basket[keys[j]]
+	})
+
+	for _, k := range keys {
+		fmt.Println(k, basket[k])
+	}
+}
